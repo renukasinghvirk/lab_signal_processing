583c69d0e79a9b48b509c09ecc8caff966b2ce07
renukasinghvirk <92505626+renukasinghvirk@users.noreply.github.com>
2025-03-05 14:54:12 +0100

    adding files manually since problems occurred from terminal

--- Lab1/utils.py
+++ Lab1/utils.py
@@ -271,13 +271,15 @@ class ParticleFilterInterface:
           # Must be valid probability distribution
           assert np.abs(self.weights.sum() - 1.0) < 1e-6, 'Weight must sum to 1.0'
     
+_tracker_ctor = {
+    'mil': cv2.legacy.TrackerMIL_create,
+    'kcf': cv2.legacy.TrackerKCF_create,
+    'tld': cv2.legacy.TrackerTLD_create,
+    'medianflow': cv2.legacy.TrackerMedianFlow_create,
+    'mosse': cv2.legacy.TrackerMOSSE_create,
+    'goturn': cv2.TrackerGOTURN_create
+}
 
-_tracker_ctor = {'mil': cv2.TrackerMIL_create,
-                 'kcf': cv2.TrackerKCF_create,
-                 'tld': cv2.TrackerTLD_create,
-                 'medianflow': cv2.TrackerMedianFlow_create,
-                 'mosse': cv2.TrackerMOSSE_create,
-                 'goturn': cv2.TrackerGOTURN_create}
     
 def create_face_tracker(name='KCF'):
     """
